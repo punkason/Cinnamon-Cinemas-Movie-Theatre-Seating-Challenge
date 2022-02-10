8f476a5ce294234a47272acfd4b9ea0d771f244a
punkason <punkason@gmail.com>
2022-02-10 22:36:43 +0000

    Create oneCustomer() Test Case

---
+++ src/test/java/com/cinemas/CinemasSeatTest.java
@@ -0,0 +1,43 @@
+package com.cinemas;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CinemasSeatTest {
+        //2D arrays seat numbers [3][5]
+
+        @Test
+        public void oneCustomer(){
+
+        }
+        //Arrange
+        Cinema cinema = new Cinema();
+        //Act
+
+        //Assert
+
+
+        /*Test Case 1
+        1 customer buy ticket
+        output should be within 1 to 3
+
+        Test Case 2
+        Remaining Seat = 2
+        the random number should be 1 or 2 but not 3
+
+        Test Case 3
+        Remaining Seat = 1
+        the random number should be 1 but not 2 or 3
+
+        Test Case 4
+        Remaining Seat = 0
+        END Program
+
+        Test Case 5
+        Test if left to right, front to back
+         */
+
+}
