ace3d00e4ff85bc2686f54462a23765590849c6a
punkason <punkason@gmail.com>
2022-02-11 17:49:10 +0000

    refactor test cases - merge printout and figure checking into one method

--- src/test/java/com/cinemas/CinemasSeatTest.java
+++ src/test/java/com/cinemas/CinemasSeatTest.java
@@ -1,6 +1,4 @@
 package com.cinemas;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,7 +8,7 @@ public class CinemasSeatTest {
         private static final int MAX_SEAT = 15;
 
         @Test
-        public void printEmptyCinema(){
+        public void noCustomer(){
                 //Arrange
                 Cinema cinema = new Cinema(0);
 
@@ -18,13 +16,12 @@ public class CinemasSeatTest {
                 cinema.buyTicket();
 
                 //Assert
-                String expectedZero = "   1  2  3  4  5 \n";
-                expectedZero += "C [Y][Y][Y][Y][Y]\n";
-                expectedZero += "B [Y][Y][Y][Y][Y]\n";
-                expectedZero += "A [Y][Y][Y][Y][Y]\n";
-
+                int expected = 0;
+                String expectedZero = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [Y][Y][Y][Y][Y]\n";
+                int noOfSeat = MAX_SEAT - cinema.getRemainingSeat();
                 String result = cinema.printSeatingPlan();
                 assertEquals(expectedZero, result);
+                assertEquals(expected, noOfSeat);
         }
 
         @Test
@@ -36,25 +33,14 @@ public class CinemasSeatTest {
                 cinema.buyTicket();
 
                 //Assert
-                int noOfSeat = MAX_SEAT - cinema.getRemainingSeat();
-                System.out.println(noOfSeat);
-                assertTrue(1 <= noOfSeat && 3 >= noOfSeat);
-        }
-
-        @Test
-        public void printOneCustomer(){
-                //Arrange
-                Cinema cinema = new Cinema(1);
-
-                //Act
-                cinema.buyTicket();
-
-                //Assert
                 String expectedOne = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][Y][Y][Y][Y]\n";
                 String expectedTwo = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][Y][Y][Y]\n";
                 String expectedThree = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][N][Y][Y]\n";
 
+                int noOfSeat = MAX_SEAT - cinema.getRemainingSeat();
                 String result = cinema.printSeatingPlan();
+
+                assertTrue(1 <= noOfSeat && 3 >= noOfSeat);
                 assertTrue(result.equals(expectedOne) || result.equals(expectedTwo) || result.equals(expectedThree));
         }
 
@@ -67,27 +53,15 @@ public class CinemasSeatTest {
                 cinema.buyTicket();
 
                 //Assert
-                int noOfSeat = MAX_SEAT - cinema.getRemainingSeat();
-                //System.out.println(noOfSeat);
-                assertTrue(2 <= noOfSeat && 6 >= noOfSeat);
-        }
-        @Test
-        public void printTwoCustomers(){
-                //Arrange
-                Cinema cinema = new Cinema(2);
-
-                //Act
-                cinema.buyTicket();
-
-                //Assert
                 String expectedTwo = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][Y][Y][Y]\n";
                 String expectedThree = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][N][Y][Y]\n";
                 String expectedFour = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][N][N][Y]\n";
                 String expectedFive = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][N][N][N]\n";
                 String expectedSix = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [N][Y][Y][Y][Y]\nA [N][N][N][N][N]\n";
-
+                int noOfSeat = MAX_SEAT - cinema.getRemainingSeat();
                 String result = cinema.printSeatingPlan();
-                System.out.println(result);
+
+                assertTrue(2 <= noOfSeat && 6 >= noOfSeat);
                 assertTrue(result.equals(expectedTwo) || result.equals(expectedThree) || result.equals(expectedFour)|| result.equals(expectedFive)|| result.equals(expectedSix));
         }
 
