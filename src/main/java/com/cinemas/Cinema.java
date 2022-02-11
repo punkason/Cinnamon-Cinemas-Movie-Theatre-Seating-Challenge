8125f23005b8804d23f6ca03a801af521ff6eb57
punkason <punkason@gmail.com>
2022-02-11 17:40:05 +0000

    Add fullHouse() - add printout comparsion

--- src/main/java/com/cinemas/Cinema.java
+++ src/main/java/com/cinemas/Cinema.java
@@ -3,13 +3,14 @@ package com.cinemas;
 public class Cinema {
     private final static int ROW = 3;
     private final static int COLUMN = 5;
-    private final static boolean[][] seat = new boolean [ROW][COLUMN];
+    private boolean[][] seat;
     private int availableTicket;
     private int noOfCustomer;
 
     public Cinema(int noOfCustomer){
         this.noOfCustomer=noOfCustomer;
         availableTicket = ROW * COLUMN;
+        seat = new boolean [ROW][COLUMN];
     }
 
     public int getRemainingSeat(){
@@ -43,7 +44,7 @@ public class Cinema {
                 ticket = (int) (Math.random() * 100 % 3) + 1;
             }
             if (availableForSale(ticket)) {
-                assignSeat(ticket);
+                 assignSeat(ticket);
             }else{
                 //System.out.println(noOfCustomer + "END");
                 return;
@@ -57,6 +58,7 @@ public class Cinema {
     }
 
     private void assignSeat(int ticket) {
+        //System.out.println(ticket);
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COLUMN; j++) {
                 if (!seat[i][j]) {
@@ -64,7 +66,6 @@ public class Cinema {
                         seat[i][j] = true;
                         ticket--;
                         availableTicket--;
-                        //System.out.println(seat[i][j] + "" + ticket);
                     }else{
                         break;
                     }
@@ -74,20 +75,24 @@ public class Cinema {
     }
 
     public String printSeatingPlan() {
-        String output = "   1  2  3  4  5 \n";
-        for (int i = ROW-1; i >= 0; i--) {
-            output += (char) (65 + i) + " ";
-            for (int j = 0; j < COLUMN; j++) {
-                output += "[";
-                if(seat[i][j])
-                    output += "N";
-                else
-                    output += "Y";
-                output += "]";
+        if (availableTicket == 0)
+            return "All tickets have been sold.";
+        else {
+            String output = "   1  2  3  4  5 \n";
+            for (int i = ROW - 1; i >= 0; i--) {
+                output += (char) (65 + i) + " ";
+                for (int j = 0; j < COLUMN; j++) {
+                    output += "[";
+                    if (seat[i][j])
+                        output += "N";
+                    else
+                        output += "Y";
+                    output += "]";
+                }
+                output += "\n";
             }
-            output += "\n";
+            //System.out.println(output);
+            return output;
         }
-        //System.out.println(output);
-        return output;
     }
 }
--- src/test/java/com/cinemas/CinemasSeatTest.java
+++ src/test/java/com/cinemas/CinemasSeatTest.java
@@ -3,27 +3,16 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CinemasSeatTest {
-        //2D arrays seat numbers [3][5]
-        private Cinema cinema;
         private static final int MAX_SEAT = 15;
-
-        /*@Before
-        public void setup(){
-
-        }*/
 
         @Test
         public void printEmptyCinema(){
                 //Arrange
-                cinema = new Cinema(0);
+                Cinema cinema = new Cinema(0);
 
                 //Act
                 cinema.buyTicket();
@@ -41,7 +30,7 @@ public class CinemasSeatTest {
         @Test
         public void oneCustomer(){
                 //Arrange
-                cinema = new Cinema(1);
+                Cinema cinema = new Cinema(1);
 
                 //Act
                 cinema.buyTicket();
@@ -55,26 +44,15 @@ public class CinemasSeatTest {
         @Test
         public void printOneCustomer(){
                 //Arrange
-                cinema = new Cinema(1);
+                Cinema cinema = new Cinema(1);
 
                 //Act
                 cinema.buyTicket();
 
                 //Assert
-                String expectedOne = "   1  2  3  4  5 \n";
-                expectedOne += "C [Y][Y][Y][Y][Y]\n";
-                expectedOne += "B [Y][Y][Y][Y][Y]\n";
-                expectedOne += "A [N][Y][Y][Y][Y]\n";
-
-                String expectedTwo = "   1  2  3  4  5 \n";
-                expectedTwo += "C [Y][Y][Y][Y][Y]\n";
-                expectedTwo += "B [Y][Y][Y][Y][Y]\n";
-                expectedTwo += "A [N][N][Y][Y][Y]\n";
-
-                String expectedThree = "   1  2  3  4  5 \n";
-                expectedThree += "C [Y][Y][Y][Y][Y]\n";
-                expectedThree += "B [Y][Y][Y][Y][Y]\n";
-                expectedThree += "A [N][N][N][Y][Y]\n";
+                String expectedOne = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][Y][Y][Y][Y]\n";
+                String expectedTwo = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][Y][Y][Y]\n";
+                String expectedThree = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][N][Y][Y]\n";
 
                 String result = cinema.printSeatingPlan();
                 assertTrue(result.equals(expectedOne) || result.equals(expectedTwo) || result.equals(expectedThree));
@@ -83,7 +61,7 @@ public class CinemasSeatTest {
         @Test
         public void twoCustomers(){
                 //Arrange
-                cinema = new Cinema(2);
+                Cinema cinema = new Cinema(2);
 
                 //Act
                 cinema.buyTicket();
@@ -96,98 +74,36 @@ public class CinemasSeatTest {
         @Test
         public void printTwoCustomers(){
                 //Arrange
-                cinema = new Cinema(2);
+                Cinema cinema = new Cinema(2);
 
                 //Act
                 cinema.buyTicket();
 
                 //Assert
-                String expectedTwo = "   1  2  3  4  5 \n";
-                expectedTwo += "C [Y][Y][Y][Y][Y]\n";
-                expectedTwo += "B [Y][Y][Y][Y][Y]\n";
-                expectedTwo += "A [N][N][Y][Y][Y]\n";
-
-                String expectedThree = "   1  2  3  4  5 \n";
-                expectedThree += "C [Y][Y][Y][Y][Y]\n";
-                expectedThree += "B [Y][Y][Y][Y][Y]\n";
-                expectedThree += "A [N][N][N][Y][Y]\n";
-
-                String expectedFour = "   1  2  3  4  5 \n";
-                expectedFour += "C [Y][Y][Y][Y][Y]\n";
-                expectedFour += "B [Y][Y][Y][Y][Y]\n";
-                expectedFour += "A [N][N][N][N][Y]\n";
-
-                String expectedFive = "   1  2  3  4  5 \n";
-                expectedFive += "C [Y][Y][Y][Y][Y]\n";
-                expectedFive += "B [Y][Y][Y][Y][Y]\n";
-                expectedFive += "A [N][N][N][N][N]\n";
-
-                String expectedSix = "   1  2  3  4  5 \n";
-                expectedSix += "C [Y][Y][Y][Y][Y]\n";
-                expectedSix += "B [N][Y][Y][Y][Y]\n";
-                expectedSix += "A [N][N][N][N][N]\n";
+                String expectedTwo = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][Y][Y][Y]\n";
+                String expectedThree = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][N][Y][Y]\n";
+                String expectedFour = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][N][N][Y]\n";
+                String expectedFive = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [Y][Y][Y][Y][Y]\nA [N][N][N][N][N]\n";
+                String expectedSix = "   1  2  3  4  5 \nC [Y][Y][Y][Y][Y]\nB [N][Y][Y][Y][Y]\nA [N][N][N][N][N]\n";
 
                 String result = cinema.printSeatingPlan();
+                System.out.println(result);
                 assertTrue(result.equals(expectedTwo) || result.equals(expectedThree) || result.equals(expectedFour)|| result.equals(expectedFive)|| result.equals(expectedSix));
-        }
-////////////////////////////////////////////////////////////////////////
-        @Ignore
-        @Test
-        public void remainingTwoSeats(){
-                //Arrange
-                cinema = new Cinema(1);
-
-                //Act
-                cinema.buyTicket();
-
-                //Assert
-                int noOfSeat = MAX_SEAT - cinema.getRemainingSeat();
-                //System.out.println(noOfSeat);
-                assertTrue(1 <= noOfSeat && 3 >= noOfSeat);
         }
 
         @Test
         public void fullHouse(){
                 //Arrange
-                cinema = new Cinema(15);
+                Cinema cinema = new Cinema(15);
 
                 //Act
                 cinema.buyTicket();
 
                 //Assert
                 int noOfSeat = MAX_SEAT - cinema.getRemainingSeat();
-                System.out.println(noOfSeat);
                 int expected = 15;
+                String expectedOutput = "All tickets have been sold.";
                 assertEquals(expected, noOfSeat);
+                assertEquals(expectedOutput, cinema.printSeatingPlan());
         }
-
-        @Test
-        public void printFullHouse(){
-                //Arrange
-                cinema = new Cinema(15);
-
-                //Act
-                cinema.buyTicket();
-
-                //Assert
-                int noOfSeat = MAX_SEAT - cinema.getRemainingSeat();
-                System.out.println(noOfSeat);
-                int expected = 15;
-                assertEquals(expected, noOfSeat);
-        }
-        /*
-        Test Case 2
-        Remaining Seat = 2
-        the random number should be 1 or 2 but not 3
-
-        Test Case 3
-        Remaining Seat = 1
-        the random number should be 1 but not 2 or 3
-
-        Test Case 4
-        Remaining Seat = 0
-        END Program
-
-         */
-
 }
